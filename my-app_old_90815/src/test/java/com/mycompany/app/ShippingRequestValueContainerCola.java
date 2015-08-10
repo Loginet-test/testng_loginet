5e66f949233bda6152e867aa34dd706e7985d5f7
MaDream <const.alexander@gmail.com>
2015-08-10 14:43:17 +0300

    morning version

---
+++ my-app_old_90815/src/test/java/com/mycompany/app/ShippingRequestValueContainerCola.java
@@ -0,0 +1,165 @@
+package com.mycompany.app;
+
+/**
+ * Created by supfe_000 on 08.08.2015.
+ */
+public class ShippingRequestValueContainerCola {
+    private String file_path = "C:\\Users\\supfe_000\\Desktop\\ColaAuto.xls";
+    private String sto;                   //   B
+    private String dateLoad;              //   D
+    private String timeLoad;              //   E
+    private String storeToLoad;           //   F
+    private String dateUnload;            //   G
+    private String timeUnload;            //   F
+    private String storeUnload;           //   I
+    private String serviceType;           //   J
+    private String vehicleType;           //   K
+    private String bodySpace;             //   L
+    private String load;                  //   M
+    private String weight;                //   N
+    private String tripTypes;             //   O
+    private String region;                //   P
+    private String cost;                  //   R
+    private String comment;               //   S
+    private String price;                 //   T
+
+    public String getFile_path() {
+        return file_path;
+    }
+
+    public void setSto(String sto) {
+        this.sto = sto;
+    }
+
+    public String getSto() {
+        return sto;
+    }
+
+    public void setDateLoad(String dateLoad) {
+        this.dateLoad = dateLoad;
+    }
+
+    public String getDateLoad() {
+        return dateLoad;
+    }
+
+    public void setTimeLoad(String timeLoad) {
+        this.timeLoad = timeLoad;
+    }
+
+    public String getTimeLoad() {
+        return timeLoad;
+    }
+
+    public void setStoreToLoad(String storeToLoad) {
+        this.storeToLoad = storeToLoad;
+    }
+
+    public String getStoreToLoad() {
+        return storeToLoad;
+    }
+
+    public void setDateUnload(String dateUnload) {
+        this.dateUnload = dateUnload;
+    }
+
+    public String getDateUnload() {
+        return dateUnload;
+    }
+
+    public void setTimeUnload(String timeUnload) {
+        this.timeUnload = timeUnload;
+    }
+
+    public String getTimeUnload() {
+        return timeUnload;
+    }
+
+    public void setStoreUnload(String storeUnload) {
+        this.storeUnload = storeUnload;
+    }
+
+    public String getStoreUnload() {
+        return storeUnload;
+    }
+
+    public void setServiceType(String serviceType) {
+        this.serviceType = serviceType;
+    }
+
+    public String getServiceType() {
+        return serviceType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setBodySpace(String bodySpace) {
+        this.bodySpace = bodySpace;
+    }
+
+    public String getBodySpace() {
+        return bodySpace;
+    }
+
+    public void setLoad(String load) {
+        this.load = load;
+    }
+
+    public String getLoad() {
+        return load;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setTripTypes(String tripTypes) {
+        this.tripTypes = tripTypes;
+    }
+
+    public String getTripTypes() {
+        return tripTypes;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setCost(String cost) {
+        this.cost = cost;
+    }
+
+    public String getCost() {
+        return cost;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+}
